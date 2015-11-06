7aafd870a71c40992ff448157b529e9d066fe817
janniks <jannik.sieb@gmail.com>
2015-11-07 00:51:50 +0100

    proximity

--- prototype/prototype/ViewController.swift
+++ prototype/prototype/ViewController.swift
@@ -11,26 +11,44 @@ import CameraManager
 
 class ViewController: UIViewController {
 	
+	private let device = UIDevice.currentDevice()
+	private let screen = UIScreen.mainScreen()
+	private var isSensorCovered = false
+	private let notificationCenter = NSNotificationCenter.defaultCenter()
+	
 	@IBOutlet weak var preview: UIView!
     @IBOutlet weak var pictureView: UIImageView!
     @IBOutlet weak var averageColorView: UIView!
     @IBOutlet weak var label: UILabel!
-    
+	@IBOutlet weak var proximityLabel: UILabel!
 	
-	@IBAction func tapButton(sender: UIButton) {
+	@IBAction func tapButton(sender: AnyObject) {
+		start()
+	}
+	
+	private func start() {
 		CameraManager.sharedInstance.addPreviewLayerToView(self.preview)
 		CameraManager.sharedInstance.cameraDevice = .Front
-        
-        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "takeImage", userInfo: nil, repeats: true)
+		
+		_ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "takeImage", userInfo: nil, repeats: true)
 	}
-    
+	
+	func proximityChanged(sender: UIDevice) {
+		isSensorCovered = device.proximityState
+		
+		if (isSensorCovered) {
+			start()
+		}
+	}
+	
     func takeImage() {
         CameraManager.sharedInstance.capturePictureWithCompletition({ (image, error) in
             if image != nil {
                 self.pictureView.image = image!
                 let averageColor = image!.average()
-                self.averageColorView.backgroundColor =  averageColor
+                self.averageColorView.backgroundColor = averageColor
                 self.label.text = "R: \(CIColor(color: averageColor).red) G: \(CIColor(color: averageColor).green) B: \(CIColor(color: averageColor).blue)"
+//				print("HUE: \())")
             }
         })
     }
@@ -38,6 +56,11 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		screen.wantsSoftwareDimming = false
+		
+		device.proximityMonitoringEnabled = true
+		notificationCenter.addObserver(self, selector: "proximityChanged:", name: UIDeviceProximityStateDidChangeNotification, object: device)
 	}
 
 	override func didReceiveMemoryWarning() {
